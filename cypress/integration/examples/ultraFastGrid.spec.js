79051f7ef7b46d1d2b2a73baa1028479940d707d
Jay Hayes <sayrilamar@gmail.com>
2021-12-01 17:51:24 -0500

    Update ultraFastGrid.spec.js

--- cypress/integration/examples/ultraFastGrid.spec.js
+++ cypress/integration/examples/ultraFastGrid.spec.js
@@ -5,7 +5,7 @@ describe("AppTest", () => {
         // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
         // but then change the above URL to https://demo.applitools.com/index_v2.html
         // (for the 2nd run)
-        cy.visit('https://demo.applitools.com');
+        cy.visit('https://demo.applitools.com/index_v2.html');
 
         // Call Open on eyes to initialize a test session
         cy.eyesOpen({
